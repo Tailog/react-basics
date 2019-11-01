390fdb7fc5aef23bcb06d03720ec423638e57639
Santos Pierre <santospierreemmanuel@gmail.com>
2019-11-01 14:15:42 +0100

    Premier Commit - Rendu Dynamique

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {
+          name: "Pierre",
+          id: 1
+        },
+        {
+          name: "Jo",
+          id: 2
+        },
+        {
+          name: "Ayse",
+          id: 3
+        }
+      ]
+    };
+  }
+  // Rendu Dynamique avec la méthode map
+  render() {
+    return (
+      <div>
+        {this.state.users.map(user => {
+          return <h1 key={user.id}>{user.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
