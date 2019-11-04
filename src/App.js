01d377896d54b8854529213cd18c3bb66bf2df76
Santos Pierre <santospierreemmanuel@gmail.com>
2019-11-04 23:40:38 +0100

    Utilisation d'Axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import {CardList} from './components/card-list/CardList'
 import { Search } from './components/search/Search';
 
+import * as axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component {
@@ -32,21 +34,45 @@ class App extends React.Component {
    * componentDidMount est disponible car nous sommes dans un composant de type class
   **/
   componentDidMount(){
-    //Fetch les datas
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(
+    //Fetch les datas basique
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then(
         // On transforme la réponse de l'API en JSON
-        (response) => response.json()
-      )
-      .then(
+    //     (response) => response.json()
+    //   )
+    //   .then(
         //On met à jour le state avec le tableau reçu du JSON
-        (users => this.setState((prevState)=>{
-          return {
+    //     (users => this.setState((prevState)=>{
+    //       return {
             // Je créer un nouveaux tableaux contenant tout les élément de mon state précedent et du tableau d'user reçu
-            users: [...prevState.users,...users]
+    //         users: [...prevState.users,...users]
+    //       }
+    //     }))
+    //   );
+    //Fetch Axios
+    // GET METHOD
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(
+      (response) => {
+        this.setState((prevState)=>{
+          return {
+            users : [...prevState.users,...response.data]
           }
-        }))
-      );
+        })
+      }
+    );
+    // POST METHOD
+    axios.post("https://jsonplaceholder.typicode.com/users",{
+      name : 'Gerard',
+      email : 'email@gmail.com'
+    })
+    .then(response => {
+      this.setState(prevState => {
+        return {
+          users: [...prevState.users, response.data]
+        };
+      });
+    });
   }
 
   /**
